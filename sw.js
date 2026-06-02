9cc665f15eafe8aa4a397c763d88086dd2ef91d3
xuliubin <53028434+xuliubin@users.noreply.github.com>
2026-06-02 20:17:24 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - 离线缓存
-const CACHE = 'wordmatch-v1';
+const CACHE = 'wordmatch-v2';
 const FILES = ['/','/index.html','/manifest.json'];
 
 self.addEventListener('install', e => {
